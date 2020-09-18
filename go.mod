ad571d539c05b5d366d36fc96ed59ee2da1900ad
Robert Philp <robert.h.philp@gmail.com>
2020-09-18 19:06:06 +0100

    Terraform test for Infinity

---
+++ go.mod
@@ -0,0 +1,5 @@
+module robertphilp.com/infinity-test
+
+go 1.15
+
+require github.com/aws/aws-lambda-go v1.19.1
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Profile struct {
+  Name		string
+  Age 		uint8
+  Hobbies 	[]string
+}
+
+// Handler is your Lambda function handler
+// It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
+// However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Processing Lambda request %t\n", request.RequestContext)
+
+	profile := Profile{"Rob", 40, []string{"Music Production", "Travel", "Movies"}}
+	json, _ := json.Marshal(profile)
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(json),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	log.Printf("Start lambda")
+	lambda.Start(Handler)
+}
